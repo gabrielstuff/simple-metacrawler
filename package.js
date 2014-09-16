e7b5650621d76e271eb6ec32181657d179cc48b8
Gabriel <gabriel@soixantecircuits.fr>
2014-09-16 23:38:41 +0200

    Update package.js

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   summary: 'Simple server method that finds images, page name and description from an url',
-  version: '0.1.4'
+  version: '0.1.4.1'
 });
 
 Package.on_use(function (api) {
-  api.use('tracker');
+  api.use('http');
   
   Npm.depends({cheerio: '0.17.0'});
 
